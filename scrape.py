72b514bd164c1e274b2592219bfc66ca896cc2f7
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-23 13:55:06 +0300

    scraper for multiple games

--- scrape.py
+++ scrape.py
@@ -3,34 +3,50 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-game_name = "Madden NFL 2004"
+games = [
+    "Madden NFL 2004",
+    "FIFA Soccer 2004",
+    "LEGO Batman: The Videogame",
+    "wwe Smackdown vs. Raw 2006",
+    "Rock Band",
+    "Space Invaders"
+]
 
 driver = webdriver.Chrome()
-search_url = f"https://www.wikidata.org/w/index.php?go=Go&search={game_name}&ns0=1&ns120=1"
-driver.get(search_url)
 
 try:
-    # Click on the first link in the search results
-    first_link = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, game_name))
-    )
-    first_link.click()
+    for game_name in games:
+        search_url = f"https://www.wikidata.org/w/index.php?go=Go&search={game_name}&ns0=1&ns120=1"
+        driver.get(search_url)
 
-    # Extract the publisher
-    publisher_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#P123 .wikibase-snakview-value a"))
-    )
-    publisher = publisher_element.text
+        try:
+            # Click on the first link in the search results under the "searchresults" div
+            first_link = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "div.mw-search-results-container a"))
+            )
+            first_link.click()
 
-    # Extract the publication date
-    date_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#P577 .wikibase-snakview-value"))
-    )
-    date = date_element.text
+            # Extract the publisher
+            publisher_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "#P123 .wikibase-snakview-value a"))
+            )
+            publisher = publisher_element.text
 
-    print(f"Game: {game_name}")
-    print(f"Publisher: {publisher}")
-    print(f"Publication Date: {date}")
+            # Extract the publication date
+            date_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "#P577 .wikibase-snakview-value"))
+            )
+            date = date_element.text
+
+            print(f"Game: {game_name}")
+            print(f"Publisher: {publisher}")
+            print(f"Publication Date: {date}")
+            print("===================================")
+
+        except Exception as e:
+            print(f"Could not retrieve information for {game_name}")
+            print(f"Error: {e}")
+            print("===================================")
 
 finally:
     driver.quit()
