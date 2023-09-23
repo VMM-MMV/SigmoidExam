13109852b4e3fe41673b0ac0d463243219d9226c
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-23 14:00:44 +0300

    scraper made into a function

--- scrape.py
+++ scrape.py
@@ -3,24 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-games = [
-    "Madden NFL 2004",
-    "FIFA Soccer 2004",
-    "LEGO Batman: The Videogame",
-    "wwe Smackdown vs. Raw 2006",
-    "Rock Band",
-    "Space Invaders"
-]
-
-driver = webdriver.Chrome()
-
-try:
-    for game_name in games:
+def find_info(game_name):
+    driver = webdriver.Chrome()
+    
+    try:
+        # Open the search URL
         search_url = f"https://www.wikidata.org/w/index.php?go=Go&search={game_name}&ns0=1&ns120=1"
         driver.get(search_url)
 
         try:
-            # Click on the first link in the search results under the "searchresults" div
+            # Click on the first link in the search results under the "mw-search-results-container" div
             first_link = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "div.mw-search-results-container a"))
             )
@@ -48,5 +40,17 @@ try:
             print(f"Error: {e}")
             print("===================================")
 
-finally:
-    driver.quit()
+    finally:
+        driver.quit()
+
+games = [
+    "Madden NFL 2004",
+    "FIFA Soccer 2004",
+    "LEGO Batman: The Videogame",
+    "wwe Smackdown vs. Raw 2006",
+    "Space Invaders",
+    "Rock Band"
+]
+
+for game in games:
+    find_info(game)
