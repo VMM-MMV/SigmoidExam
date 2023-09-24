40cf6d4a00a2364df42c15e09324014dd0b2e437
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-24 12:03:20 +0300

    selenium scraper appender to csv

--- dataframe_editor.py
+++ dataframe_editor.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from selenium_scrape import *
+
+driver = webdriver.Chrome()
 
 missing_rows = pd.read_csv("MissingRows.csv")
 
@@ -8,3 +11,18 @@ for index, row in missing_rows.iterrows():
     publisher = row['Publisher']
 
     print(f'name = {name}, publishing year = {year}, publisher = {publisher}')
+    
+    if pd.isna(year) or pd.isna(publisher):
+        info = find_info(name + " video game", driver)
+        print(info)
+        print()
+        if 'Year_of_Release' in info and pd.isna(year):
+            missing_rows.at[index, 'Year_of_Release'] = int(info['Year_of_Release'])
+        if 'Publisher' in info and pd.isna(publisher):
+            missing_rows.at[index, 'Publisher'] = str(info['Publisher'])
+
+print(missing_rows)
+
+missing_rows.to_csv('FilledRows.csv')
+
+driver.quit()
